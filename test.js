4676b03f845c21337f0b4f948e5b18a5b0415107
Alexey Demin <deminn@gmail.com>
2016-10-06 16:59:04 -0300

    Basically I rewrite the code to be more readable and clear.

--- test.js
+++ test.js
@@ -1,109 +1,87 @@
 'use strict';
 
-// This class is used for logins
 class Login {
-  constructor(hash) {
+  constructor(users) {
     this.sessions = [];
-    this.users = [];
-    this.passwords = [];
-    Object.keys(hash).map(k => ({k, v: hash[k]})).map(e => {
-      this.users = this.users.concat([e.k]);
-      this.passwords = this.passwords.concat([e.v]);
-    });
+    this.users = users;
   }
 
-  logout(user) {
-    this.sessions.forEach((session, i) => {
-      if (session === user) {
-        this.sessions[i] = null;
-      }
-    });
-    this.sessions = this.sessions.filter(session => session !== null);
+  logout(username) {
+    this.sessions = this.sessions.filter(x => x !== username);
   }
 
-  // Checks if user exists
-  userExists(user) {
-    // Temp variable for storing the user if found
-    let temp = '';
-    for (let i of this.users) {
-      if (i === user) {
-        temp = user;
-      }
+  userExists(username) {
+    let user = this.getUser(username);
+    return typeof user !== 'undefined';
+  }
+
+  registerUser(user) {
+    this.users.push(user);
+  }
+
+  removeUser(username) {
+    this.users = this.users.filter(x => x.username !== username);
+  }
+
+  checkPassword(username, password) {
+    let user = this.getUser(username);
+    return user.pass === password;
+  }
+
+  getUser(username) {
+    return this.users.find(x => x.username === username);
+  }
+
+  updatePassword(username, oldPassword, newPassword) {
+    if (!this.userExists(username))
+      return false;
+    if (!this.checkPassword(username, oldPassword))
+      return false;
+    let user = this.getUser(username);
+    user.pass = newPassword;
+    this.removeUser(username);
+    this.registerUser(user);
+  }
+
+  login(username, password) {
+    let user = this.getUser(username);
+    if (user.pass === password) {
+      this.sessions.push(username);
     }
-    let exists = (temp !== '' && temp === user);
-    return exists;
-  }
-
-  // Register user
-  registerUser(user, password) {
-    let lastIndex = this.users.length;
-    this.users[lastIndex] = user;
-    this.passwords[lastIndex] = password;
-  }
-
-  removeUser(user) {
-    let index = this.idx(user, this.users);
-    this.users[index] = null;
-    this.passwords[index] = null;
-    this.users = this.users.filter(user => user !== null);
-    this.passwords = this.passwords.filter(password => password !== null);
-  }
-
-  checkPassword(user, password) {
-    let index = this.idx(user, this.users);
-    let passwordCorrect = this.passwords[index] === password;
-    return passwordCorrect;
-  }
-
-  updatePassword(user, oldPassword, newPassword) {
-    // First we check if the user exists
-    let user1 = '';
-    for (let i of this.users) {
-      if (i === user) {
-        user1 = user;
-      }
-    }
-    if (user1 === user) {
-      let index = this.idx(user, this.users);
-      if (this.passwords[index] === oldPassword) {
-        this.passwords[index] = newPassword;
-        return true;
-      }
-    }
-    return false;
-  }
-
-  login(user, password) {
-    let index = this.idx(user, this.users);
-    if (this.passwords[index] === password) {
-      this.sessions.push(user);
-    }
-  }
-
-  // Gets index of an element in an array
-  idx(element, array) {
-    let cont=0;
-    for (let i of array) {
-      if (i === element) {
-        return cont;
-      }
-      cont += 1;
-    }
-    return cont;
   }
 }
 
-let registeredUsers = {
-  user1: 'pass1',
-  user2: 'pass2',
-  user3: 'pass3'
-};
+
+let registeredUsers = [
+  {username: 'user1', pass: 'pass1'},
+  {username: 'user2', pass: 'pass2'},
+  {username: 'user3', pass: 'pass3'}
+];
 
 let login = new Login(registeredUsers);
+console.log( login.users );
 
-login.registerUser('user4', 'pass4');
-login.login('user4', 'pass4');
-login.updatePassword('user3', 'pass3', 'pass5');
-login.login('user3', 'pass5');
-login.logout('user4');
-login.logout('user3');
+login.registerUser({username:'user4', pass:'pass4'});
+console.log( login.users );
+
+login.removeUser('user2');
+console.log( login.users );
+
+console.log( login.checkPassword('user3','pass3') );
+console.log( login.checkPassword('user3','pass3zzz') );
+console.log( login.sessions );
+login.login('user1', 'pass1');
+login.login('user3', 'pass3');
+login.login('user4', 'pass4zz');
+console.log( login.sessions );
+login.logout('user1');
+console.log( login.sessions );
+
+login.updatePassword('user3', 'pass3', 'pass333');
+console.log( login.sessions );
+console.log( login.users );
+login.updatePassword('user0', 'pass', 'pass');
+console.log( login.users );
+login.updatePassword('user1', 'pass1zz', 'pass111');
+console.log( login.users );
+
